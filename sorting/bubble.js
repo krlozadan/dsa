e5ebb2ad22e3305f4b87888ea9019e1f31ebaa3f
krlozadan <krlozadan@gmail.com>
2025-04-05 13:05:37 -0700

    docs: bubble sort algorithm description

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,15 +1,26 @@
-function bubbleSort(array) {
+/*
 
-    let sorted;
+Bubble sort
+Simple sorting algorithm that steps throught the list, compares adjacent numbers
+and swaps them if they are in the wrong order. Bubbling the largest element to
+the end of the list
+
+Time complexity O(n^2) making it relatively inefficient for large datasets
+Space complexity O(1)
+
+*/
+
+function bubbleSort(array) {
+    let sorted, tmp;
     let rightEndIndex = array.length - 1;
     do {
         sorted = true;
         for (let i = 0; i < rightEndIndex; i++) {
             if (array[i] > array[i + 1]) {
                 sorted = false;
-                const temp = array[i + 1];
+                tmp = array[i + 1];
                 array[i + 1] = array[i];
-                array[i] = temp;
+                array[i] = tmp;
             }
         }
         rightEndIndex--;
